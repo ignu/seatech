853fb1527d668389ebf68ac763c23e79eb8325af
Len Smith <len@barrison.com>
2017-12-30 22:05:54 -0800

    Actually get Meetup event

--- api/lib/scrapers/meetup.ex
+++ api/lib/scrapers/meetup.ex
@@ -1,3 +1,10 @@
 defmodule Api.Scrapers.Meetup do
+  def fetch(meetup_name) do
+    host = "https://api.meetup.com"
+    url = "#{host}/#{meetup_name}/events"
 
+    {:ok, %{body: body}} = HTTPoison.get(url)
+
+    body |> Poison.decode!
+  end
 end
--- api/test/scrapers/meetup_test.exs
+++ api/test/scrapers/meetup_test.exs
@@ -1,7 +1,9 @@
 defmodule Api.Scrapers.MeetupTest do
   use ExUnit.Case
+  alias Api.Scrapers.Meetup
 
-  test "it can get " do
-    assert 1 == 1
+  test "it can get a list of meetups " do
+    events = Meetup.fetch("The-Seattle-Alternative-Boardgamers-Meetup-Group")
+    assert events |> Enum.any?
   end
 end
